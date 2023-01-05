b02df3606ec9e7eb2094c5d5a1580e7089e62630
Camila Lima <camila.bruschetta@ab-inbev.com>
2023-01-04 22:54:16 -0300

    Continuando Projeto AluraMidi

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-function takeMusicPom() {
+// function takeMusicPom() {
+//   document.querySelector("#som_tecla_pom").play();
+// }
+
+// const keyList = document.querySelectorAll(".tecla");
+
+// keyList[0].onClick = takeMusicPom;
+
+// Pom ----------------------------------------
+function tocaSomPom() {
   document.querySelector("#som_tecla_pom").play();
 }
 
-document.querySelectorAll(".tecla");
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+//enquanto
+while (0 <= 9) {
+  listaDeTeclas[0].onclick = tocaSomPom;
+}
