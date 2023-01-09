97e1e4bd92ba1a3910c29c2858f1105e17165049
Camila Lima <camila.bruschetta@ab-inbev.com>
2023-01-08 22:17:20 -0300

    Implementando Projeto AluraMidi utilizando For

--- main.js
+++ main.js
@@ -7,15 +7,45 @@
 // keyList[0].onClick = takeMusicPom;
 
 // Pom ----------------------------------------
-function tocaSomPom() {
-  document.querySelector("#som_tecla_pom").play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-listaDeTeclas[0].onclick = tocaSomPom;
+// let contador = 0;
 
 //enquanto
-while (0 <= 9) {
-  listaDeTeclas[0].onclick = tocaSomPom;
+// while (contador < listaDeTeclas.length) {
+//   const tecla = listaDeTeclas[contador];
+
+//   const instrumento = tecla.classList[1];
+
+// console.log(instrumento);
+
+//template string
+// const idAudio = `#som_${instrumento}`;
+
+// tecla.onclick = function () {
+//   tocaSom(idAudio);
+// };
+
+// contador = contador + 1;
+
+// console.log(contador);
+// }
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`; //
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  contador = contador + 1;
 }
